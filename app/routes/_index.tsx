27c35369fe5745013da419823e3abbbb26c7ead1
DeCaelo <ludovic.cleuet@gmail.com>
2024-02-10 19:07:36 +0100

    switch styling

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,8 +10,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <Switch.Root className=" h-5 w-5 bg-white data-[state=checked]:bg-emerald-500">
-      <Switch.Thumb />
+    <Switch.Root className="w-11 rounded-full bg-gray-800 p-px shadow-inner shadow-black/50 transition duration-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400 active:bg-gray-600 data-[state=checked]:bg-sky-500 active:data-[state=checked]:bg-sky-400">
+      <Switch.Thumb className="block h-6 w-6 rounded-full bg-gray-200 shadow-sm transition duration-500 data-[state=checked]:translate-x-[18px] data-[state=checked]:bg-white" />
     </Switch.Root>
   );
 }
